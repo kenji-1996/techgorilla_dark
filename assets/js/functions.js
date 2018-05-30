361ae99b26bd5352fac337702251a23821ebc3a7
Callum Chisholm <callum@techgorilla.io>
2018-05-31 09:32:49 +1000

    Update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -86,18 +86,15 @@
                 that.tick();
             }, delta);
         };
-
-        window.onload = function() {
-            var elements = document.getElementsByClassName('typewrite');
-            for (var i=0; i<elements.length; i++) {
-                console.log(i);
-                var toRotate = elements[i].getAttribute('data-type');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtType(elements[i], JSON.parse(toRotate), period);
-                }
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            console.log(i);
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
             }
-        };
+        }
 
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
